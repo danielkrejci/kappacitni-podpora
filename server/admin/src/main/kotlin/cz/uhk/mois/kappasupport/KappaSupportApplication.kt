1c7ce3b1a18a088c683774473e8f20646cf967bc
Jan Chaloupka <chaloja1@uhk.cz>
2023-03-01 21:52:34 +0100

    admin init

---
+++ server/admin/src/main/kotlin/cz/uhk/mois/kappasupport/KappaSupportApplication.kt
@@ -0,0 +1,11 @@
+package cz.uhk.mois.kappasupport
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KappaSupportApplication
+
+fun main(args: Array<String>) {
+	runApplication<KappaSupportApplication>(*args)
+}
