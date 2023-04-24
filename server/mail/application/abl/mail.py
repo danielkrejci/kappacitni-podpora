b1ca34a8a182eaf2da7a985b6dfb614af21024e4
David Tláskal <david.tlaskal@unicorn.com>
2023-04-24 20:04:51 +0200

    Find template using glob, etc...

--- server/mail/application/abl/mail.py
+++ server/mail/application/abl/mail.py
@@ -1,3 +1,4 @@
+import config
 import jinja2
 import html5lib
 from typing import *
@@ -11,6 +12,11 @@ from application.app import templates
 def render_email_template(template_name: str, template_context: Dict[str, Any]) -> str:
     """ Render email html """
     try:  # Get template from templates environment, if template exists
+        template_full_path = next(config.templates_path.glob(f'{template_name}*'), None)
+        if not template_full_path:
+            raise jinja2.TemplateNotFound(template_name)
+        template_name = str(template_full_path.relative_to(config.templates_path))
+
         template = templates.env.get_template(template_name)
     except jinja2.TemplateNotFound:
         raise HTTPException(status.HTTP_400_BAD_REQUEST, 'Template not found.')
